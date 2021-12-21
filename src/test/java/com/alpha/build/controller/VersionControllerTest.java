6eb35b85a122834aee640d3cb4c8d2a5cee98b35
Allan Thrane Andersen <einheriii@gmail.com>
2021-12-21 11:17:45 +0000

    Add unit test to VersionController #7

---
+++ src/test/java/com/alpha/build/controller/VersionControllerTest.java
@@ -0,0 +1,30 @@
+package com.alpha.build.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit test of the {@link Application} class.
+ */
+@SpringBootTest
+public class VersionControllerTest {
+    
+    /**
+     * Class under test.
+     */
+    @Autowired
+	private VersionController controller;
+
+    /**
+     * Test that the Spring context is creating the controller.
+     * 
+     * @throws Exception if test fails.
+     */
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}    
+}
